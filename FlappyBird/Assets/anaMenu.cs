f3920d05b5369b3479f7c59f3a670bac6c5bd41a
hasanaydin7 <hsnydn77@gmail.com>
2022-08-03 22:09:13 +0200

    First Unity Engine Project
    This is my first beginner unity engine project. It is also my final project.

---
+++ FlappyBird/Assets/anaMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class anaMenu : MonoBehaviour
+{
+    public Text puanText;
+    public Text puan;
+    void Start()
+    {
+        int enYuksekPuan = PlayerPrefs.GetInt("enYuksekPuanKaydi");
+        int puanGelen = PlayerPrefs.GetInt("puanKaydi");
+
+        puanText.text = "En Yuksek Puan = " + enYuksekPuan;
+        puan.text = "Puan = " + puanGelen;
+    }
+
+   
+    void Update()
+    {
+        
+    }
+    public void oyunaGit()
+    {
+        SceneManager.LoadScene("basla");
+    }
+    public void oyundanCik()
+    {
+        Application.Quit();
+    }
+}
---
+++ FlappyBird/Assets/kontrol.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class kontrol : MonoBehaviour
+{
+    public Sprite []kusSprite;
+    SpriteRenderer spriteRenderer;
+    bool ileriGeriKontrol = true;
+    int kusSayac = 0;
+    float kusAnimasyonZaman = 0;
+
+    Rigidbody2D fizik;
+
+    int puan = 0;
+
+    public Text puanText;
+
+    bool oyunBitti = true;
+
+    oyunKontrol oyunKontrol;
+
+    AudioSource []sesler;
+
+    int enYuksekPuan = 0;
+
+    
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        fizik = GetComponent<Rigidbody2D>();
+        oyunKontrol = GameObject.FindGameObjectWithTag("oyunkontrol").GetComponent<oyunKontrol>();
+        sesler=GetComponents<AudioSource>();
+        enYuksekPuan = PlayerPrefs.GetInt("enYuksekPuanKaydi");
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && oyunBitti)
+        {
+            fizik.velocity = new Vector2(0,0);
+            fizik.AddForce(new Vector2(0,200));
+            sesler[0].Play();
+            
+        }
+        if(fizik.velocity.y>0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 45);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, -45);
+        }
+        Animasyon();
+    }
+    void Animasyon()
+    {
+        kusAnimasyonZaman += Time.deltaTime;
+        if (kusAnimasyonZaman > 0.2f)
+        {
+            kusAnimasyonZaman = 0;
+            if (ileriGeriKontrol)
+            {
+                spriteRenderer.sprite = kusSprite[kusSayac];
+                kusSayac++;
+                if (kusSayac == kusSprite.Length)
+                {
+                    kusSayac--;
+                    ileriGeriKontrol = false;
+                }
+            }
+            else
+            {
+                kusSayac--;
+                spriteRenderer.sprite = kusSprite[kusSayac];
+                if (kusSayac == 0)
+                {
+                    kusSayac++;
+                    ileriGeriKontrol = true;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag=="puan")
+        {
+            puan++;
+            puanText.text = "puan =" + puan;
+            sesler[1].Play();
+            
+        }
+        if(col.gameObject.tag=="engel")
+        {
+            oyunBitti = false;
+            sesler[2].Play(); 
+            oyunKontrol.oyunBitti();
+            GetComponent<CircleCollider2D>().enabled = false;
+
+            if(puan>enYuksekPuan)
+            {
+                enYuksekPuan = puan;
+                PlayerPrefs.SetInt("enYuksekPuanKaydi", enYuksekPuan);
+            }
+            Invoke("anaMenuyeDon", 2);
+        }
+    }
+    void anaMenuyeDon()
+    {
+        PlayerPrefs.SetInt("puanKaydi",puan);
+        SceneManager.LoadScene("anaMenu");
+    }
+}
